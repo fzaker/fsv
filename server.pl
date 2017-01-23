b903e2f50202eb8670bc032dadb32e29507e599d
Farshid Zaker <farshid@backbase.com>
2017-01-23 14:22:42 +0100

    basic rest service

---
+++ server.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+use Mojolicious::Lite;
+
+my $default_handler = sub {
+    my $c = shift;
+    my %hash = (
+        "apple"  => "red",
+        "orange" => "orange",
+        "grape"  => "purple",
+        "nested" => {
+            "one" => "1",
+            "two" => "2"
+        }
+    );
+    $c->render(json => \%hash);
+};
+
+my %route = ('/' => $default_handler);
+
+get(%route);
+
+app->start;
